767c4915902a4ed3c1488db3c0125a15a3a08ee0
EmmanuelNanaGyanAsare <2425402212@live.gctu.edu.gh>
2026-05-31 15:59:14 -0700

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: EMMANUEL NANA GYAN ASARE
+# STUDENT ID:  2425402212
+# Email: 2425402212@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, EMMANUEL NANA GYAN ASARE, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print small initials and large block letter initials for E, N, G
 
+    print()
+    print("...ENG")
+    print()
+    print("...EEEEEEEEEE..NN.......NN..GGGGGGGGGG.****")
+    print("...EE..........NNN......NN..GG.........****")
+    print("...EE..........NNNN.....NN..GG.............")
+    print("...EEEEEEEE....NN.NN....NN..GG..GGGGG..****")
+    print("...EEEEEEEE....NN..NN...NN..GG..GGGGG..****")
+    print("...EE..........NN...NN..NN..GG.....GG.....*")
+    print("...EE..........NN....NN.NN..GG.....GG.....*")
+    print("...EEEEEEEEEE..NN.....NNNN..GGGGGGGGGG..***")
+    print("...EEEEEEEEEE..NN......NNN..GGGGGGGGGG..***")
+    print("...EEEEEEEEEE..NN.......NN..GGGGGGGGGG..***")
+    print()
 
 
 main()
